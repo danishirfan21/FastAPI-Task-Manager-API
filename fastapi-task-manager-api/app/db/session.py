280522caa6e376890193224a5b310be7caefc568
danishirfan21 <44131991+danishirfan21@users.noreply.github.com>
2025-10-07 00:43:26 +0500

    fix

--- fastapi-task-manager-api/app/db/session.py
+++ fastapi-task-manager-api/app/db/session.py
@@ -1,7 +1,9 @@
 
 import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine
 
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
